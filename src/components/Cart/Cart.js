6cbc8a2f37e2b10d46e12e5e29590c60c7010f73
Doan Van Hoang <hoangusa08@gmail.com>
2021-05-19 13:14:22 +0700

    web client update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import API from '../Config/Api';
 import NumberFormat from 'react-number-format';
