374e6454cb01133746eae8f24b56ea0ae168ff29
Doan Van Hoang <hoangusa08@gmail.com>
2021-08-28 07:41:01 +0700

    fix order Item

--- src/components/MyAccount/Order/OrderItem.js
+++ src/components/MyAccount/Order/OrderItem.js
@@ -1,20 +1,14 @@
-import React, { Component, useEffect, useState } from 'react';
-// import { Link } from "@reach/router";
-import NumberFormat from 'react-number-format';
-import config from '../../Config/Config';
+import React from 'react';
 import { useHistory } from 'react-router';
 function OrderItem(props) {
     const { order } = props;
     // console.log(order);
     const history = useHistory()
-    const view = (e) => {
-        // console.log(e.target)
-    } 
     return (
         <tr>
             <td>{order.id}</td>
             <td>{order.total_Money}</td>
-            <td>{(order.total_Money == true) ? "Paid" : "UnPaid"}</td>
+            <td>{(order.is_paid == true) ? "Paid" : "UnPaid"}</td>
             <td><button id={order.id} className="btn" onClick ={ e=> {history.push(`/view-order/${order.id}`)}}>View</button></td>
         </tr>
     )
